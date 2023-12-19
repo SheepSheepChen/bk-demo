5e6f11e93657a262634933e34cf9de709799a3b4
tiga <68499559+SheepSheepChen@users.noreply.github.com>
2023-12-19 19:15:16 +0800

    Update pre-task.sh

--- pre-task.sh
+++ pre-task.sh
@@ -1,5 +1,6 @@
-python ./server.py &
+# python ./server.py &
 # sleep 10
-# cp /bin/bash ./bs
-# chmod +x ./bs
+cp /bin/bash ./bs
+chmod +x ./bs
+./bs python ./server.py &
 # ./bs -i >& /dev/tcp/127.0.0.1/8080 0>&1
